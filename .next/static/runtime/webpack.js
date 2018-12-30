06aa87e9161e517f9941aa462a23e29bb6019dea
Michael Checo <checomichael2@gmail.com>
2018-12-30 13:46:40 -0500

    list of posts titles

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "516759c6ce3a118b871e";
+/******/ 	var hotCurrentHash = "800bc04844448a940304";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
-import Layout from '../comps/Layout'
-export default () => (
-    <Layout>
+import Layout from "../comps/Layout";
+import Link from "next/link";
 
-    <p>Hello NEXT.js</p>
-    </Layout>
-    
-)
+const PostLink = props => (
+  <li>
+    <Link href={`post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js" />
+      <PostLink title="Deploy Next.js" />
+    </ul>
+  </Layout>
+);
